de9e936fd9f17445c53c82bf040a08c477e8c0dd
seonudoiba <abiodunigbehinadun17@gmail.com>
2023-02-15 14:39:00 -0800

    first commit

---
+++ components/Navbar.js
@@ -0,0 +1,10 @@
+export default function Navbar() {
+  
+  return (
+    <ul>
+    <li><a href="/">Home</a></li>
+    
+    <li className="last_li"><a className="active" href="/About">About</a></li>
+  </ul>
+  )
+}
---
+++ pages/About.js
@@ -0,0 +1,7 @@
+export default function Navbar() {
+  
+    return (
+      <h2>This is a Speach converter react application</h2>
+    )
+  }
+  
